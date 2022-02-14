b6fba7dad744b779b856e3d373a698e7e2d93e86
Emre Demir <emre.demir@adesso.com.tr>
2022-02-14 21:21:22 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Demir, Emre on 14.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
